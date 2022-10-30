e86953587b2a35bb61e6da82d59bf8c88a163d70
Doyeon Yun <61214927+govlehdus@users.noreply.github.com>
2022-10-30 01:43:40 -0700

    Update 125. Valid Palindrome.py

--- leetcode/125. Valid Palindrome.py
+++ leetcode/125. Valid Palindrome.py
@@ -3,3 +3,19 @@ class Solution:
         
         b = ''.join(x for x in s if x.isalnum()).lower()
         return b == b[::-1]
+
+#Using Two pointers
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l,r = 0, len(s)-1
+        while r>l:    
+            while s[l].isalnum() == False and l<r:
+                l += 1
+            while s[r].isalnum() == False and l<r:
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            else:
+                l +=1
+                r -=1
+        return True
