3169c32b1850cb5354b5b626a8f5595aa2d5baa7
Doyeon Yun <61214927+govlehdus@users.noreply.github.com>
2023-07-18 22:05:51 +0900

    Update 217. Contains Duplicate.py

--- leetcode/217. Contains Duplicate.py
+++ leetcode/217. Contains Duplicate.py
@@ -1,3 +1,9 @@
+#I will make a set of nums and check the length of the set and list
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        dup = set(nums)
+        return len(dup) != len(nums)
+#This has better efficiency since instead of making a full set, it adds one by one and return True if one element is already inside the set.
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         hashset = set()
