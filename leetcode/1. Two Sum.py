25b3bc583daa16b745b1df566ab72c2c0e5a51ad
Doyeon Yun <61214927+govlehdus@users.noreply.github.com>
2023-07-18 22:18:44 +0900

    Update 1. Two Sum.py

--- leetcode/1. Two Sum.py
+++ leetcode/1. Two Sum.py
@@ -1,6 +1,17 @@
+#2 for loop which is expensive
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
-            key = target - nums[i]
-            if key in nums and nums.index(key) != i:
-                return [i,nums.index(key)]
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+# This has better memory and speed. iterate one loop and putting the element's value and index to use it in the future                  
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {}  # val -> index
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
