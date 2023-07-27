d6f9f9aaa55616c99a3c5ef500b38ad5ca3b8182
Doyeon Yun <61214927+govlehdus@users.noreply.github.com>
2023-07-27 20:27:33 +0900

    Update 238. Product of Array Except Self.py

--- leetcode/238. Product of Array Except Self.py
+++ leetcode/238. Product of Array Except Self.py
@@ -1,12 +1,15 @@
-class Solution:
+"""Since I can't use division, i need to make a list of elements multiplication from left to right and right to left, 
+so I can multiply the element that is behind me and next to me to get the product of array except self.class 
+"""
+Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        a=[1]
-        b=[1]
-        
-        ans = []
-        for i in range(1, len(nums)):
-            a.append(nums[i-1]*a[i-1])
-            b.append(nums[-i] * b[i-1])
-        for j in range(len(nums)):
-            ans.append(a[j] * b[-1-j])
-        return ans
+        res = [0] * len(nums)
+        pre = [1] * len(nums)
+        suf = [1] * len(nums)
+        for i in range(1,len(nums)):
+            pre[i] = pre[i-1] * nums[i-1]
+        for i in range(len(nums)-1,0,-1):
+            suf[len(nums)-i] = suf[len(nums)-i-1] * nums[i]
+        for i in range(len(nums)):
+            res[i] = pre[i] * suf[-(i+1)]
+        return res
