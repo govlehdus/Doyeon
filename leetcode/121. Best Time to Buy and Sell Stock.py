b26130bccc5c1d693195f772e7d8b0bf6f2d085b
Doyeon Yun <61214927+govlehdus@users.noreply.github.com>
2023-08-08 15:01:06 +0900

    Update 121. Best Time to Buy and Sell Stock.py

--- leetcode/121. Best Time to Buy and Sell Stock.py
+++ leetcode/121. Best Time to Buy and Sell Stock.py
@@ -1,10 +1,15 @@
+"""Since it is getting the smallest value and highest value after getting the smallest value to get the maximum profit, so I choose to keep track of two variables
+low and high. Using sliding window algorithm, I updated the low and high when it meets the condition.
+"""
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        low,high = float("inf"),0
         res = 0
-
-        l = 0
-        for r in range(1, len(prices)):
-            if prices[r] < prices[l]:
-                l = r
-            res = max(res, prices[r] - prices[l])
+        for i in prices:
+            if i < low:
+                low = i
+                high = i
+            elif i > high:
+                high = i
+                res = max(res, high-low)
         return res
