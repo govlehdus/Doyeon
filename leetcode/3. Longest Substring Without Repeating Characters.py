9e68e05cd74d040fa588394a96807608d4470f19
Doyeon Yun <61214927+govlehdus@users.noreply.github.com>
2023-08-08 15:21:11 +0900

    Update 3. Longest Substring Without Repeating Characters.py

--- leetcode/3. Longest Substring Without Repeating Characters.py
+++ leetcode/3. Longest Substring Without Repeating Characters.py
@@ -1,14 +1,27 @@
+"""To measure the longest substring, I use sliding window algorithm to put the max substrings and remove the first element until it is the same as new element to put.
+To do that fast, I choose to build a deque which is first in first out, so it is fast to pop the first element"""
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        l, cnt = 0,0
-        string = ''
+        q = collections.deque()
+        res = 0
         for i in s:
-            if i not in string:
-                string += i
-                cnt = max(cnt, len(string))
+            if i not in q:
+                q.append(i)
             else:
-                while string[0] != i :
-                    string = string[1:]
-                string = string[1:]
-                string += i
-        return cnt
+                while i in q:
+                    q.popleft()
+                q.append(i)
+            res = max(res, len(q))
+        return res
+#In a shorter code
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        q = collections.deque()
+        res = 0
+        for i in s:
+            while i in q:
+                q.popleft()
+            q.append(i)
+            res = max(res, len(q))
+        return res
+            
