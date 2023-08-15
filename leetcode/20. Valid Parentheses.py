a04313979c29b7e7aa6a9a86e9ce5adee39d4365
Doyeon Yun <61214927+govlehdus@users.noreply.github.com>
2023-08-15 18:01:46 +0900

    Update 20. Valid Parentheses.py

--- leetcode/20. Valid Parentheses.py
+++ leetcode/20. Valid Parentheses.py
@@ -1,12 +1,17 @@
+"""First there are three conditions, it is opening parenthesis, it is closing parenthesis that has opening parenthesis in front, and it is closing parenthesis that
+doesn't have matching opening parenthesis. Using stack, since it is LIFO, I make a condition for the opening parenthesis and closing parenthesis."""
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        di = {']':'[', '}':'{',')':'('}
-        for p in s:
-            if p not in di:
-                stack.append(p)
-                continue
-            if not stack or stack[-1] != di[p]:
-                return False
-            stack.pop()
-        return True if len(stack) == 0 else False
+        opens = {')': '(', '}': '{', ']': '['}
+        for i in s:
+            if i not in opens:
+                stack.append(i)
+            else:
+                if not stack or stack[-1] != opens[i]:
+                    return False
+                else:
+                    stack.pop()
+        return not stack
+
+            
