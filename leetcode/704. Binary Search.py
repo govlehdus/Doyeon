7a5a21a3cf14732c2d49e4a5ef3c17c8897f6ddb
Doyeon Yun <61214927+govlehdus@users.noreply.github.com>
2023-10-23 15:31:46 -0700

    Update 704. Binary Search.py

--- leetcode/704. Binary Search.py
+++ leetcode/704. Binary Search.py
@@ -1,12 +1,14 @@
+#using binary search, keep on checking it. Put the return mid into else, since it is the least thing to happen.
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l,r = 0, len(nums)-1
-        while l <=r:
-            m = (l+r) //2
-            if nums[m] == target:
-                return m
-            elif nums[m] > target:
-                r = m-1
+        left, right = 0 ,len(nums)-1
+        while left<= right:
+            mid = left + ((right-left)//2) #if i do (left + right)//2, it can lead to overflow.
+            if nums[mid] > target:
+                right = mid-1
+            elif nums[mid] <target:
+                left = mid +1
             else:
-                l = m +1
+                return mid
         return -1
+        
