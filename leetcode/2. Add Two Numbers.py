9b28c167150c645618ca8a9958c6f42d4fd7396c
Doyeon Yun <61214927+govlehdus@users.noreply.github.com>
2023-11-28 21:48:17 -0800

    Create 2. Add Two Numbers.py

---
+++ leetcode/2. Add Two Numbers.py
@@ -0,0 +1,25 @@
+#checking it has carry, l1, and l2, and add it. consider the edge cases where one of the lists has contains more numbers, or 8 +7 that leads 15 where the there is no 10th number in list
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        cur = dummy
+        carry = 0
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+            
+            val = v1 + v2 + carry
+            carry = val // 10
+            val = val % 10
+            cur.next = ListNode(val)
+            cur = cur.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return dummy.next
