4d134884d6741b431df19f5e691ed3a2cf783b45
Doyeon Yun <61214927+govlehdus@users.noreply.github.com>
2023-11-30 22:21:43 -0800

    Update 226. Invert Binary Tree.py

--- leetcode/226. Invert Binary Tree.py
+++ leetcode/226. Invert Binary Tree.py
@@ -1,3 +1,4 @@
+#By simply change left and right child of root. Check the edge case where there is no root.
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
