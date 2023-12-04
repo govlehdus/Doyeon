5b6044607bd278d71386231a06613685cacfcce6
Doyeon Yun <61214927+govlehdus@users.noreply.github.com>
2023-12-04 00:02:23 -0800

    Update 199. Binary Tree Right Side View.py

--- leetcode/199. Binary Tree Right Side View.py
+++ leetcode/199. Binary Tree Right Side View.py
@@ -1,3 +1,5 @@
+#Having a val that is the list of all the nodes' value in the same level, and add its' last element, so it will be the same as the right side view.
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,21 +8,21 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        if not root:
-            return root
-
-        queue =[]
         res = []
-        queue.append(root)
+        stack = []
+        if root:
+            stack.append(root)
 
-        while queue:
-            ans = []
-            for i in range(len(queue)):
-                node = queue.pop(0)
-                ans.append(node.val)
+        while stack:
+            val = []
+
+            for i in range(len(stack)):
+                node = stack.pop(0)
+                val.append(node.val)
                 if node.left:
-                    queue.append(node.left)
+                    stack.append(node.left)
                 if node.right:
-                    queue.append(node.right)
-            res.append(ans[-1])
-        return res    
+                    stack.append(node.right)
+            res.append(val[-1])
+        return res
+        
