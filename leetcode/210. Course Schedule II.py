952be540ef3dfe41d2c72a318f9e298c51b5b505
Doyeon Yun <61214927+govlehdus@users.noreply.github.com>
2023-12-21 18:05:01 -0800

    Create 210. Course Schedule II.py

---
+++ leetcode/210. Course Schedule II.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        preq = {i:set() for i in range(numCourses)}
+        graph = collections.defaultdict(set)
+        for i,j in prerequisites:
+            preq[i].add(j)
+            graph[j].add(i)
+        
+        q = collections.deque([])
+        for k, v in preq.items():
+            if len(v) == 0:
+                q.append(k)
+        taken = []
+        while q:
+            course = q.popleft()
+            taken.append(course)
+            if len(taken) == numCourses:
+                return taken
+            for cor in graph[course]:
+                preq[cor].remove(course)
+                if not preq[cor]:
+                    q.append(cor)
+        return []
+        
