5105e997de475163f4ff9a56f49001a144a09c3d
Doyeon Yun <61214927+govlehdus@users.noreply.github.com>
2024-01-13 22:53:09 -0800

    Update 322. Coin Change.py

--- leetcode/322. Coin Change.py
+++ leetcode/322. Coin Change.py
@@ -1,9 +1,15 @@
-class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [amount + 1] * (amount+1)
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        dp = [float("inf")] * (amount+1)
         dp[0] = 0
-        for i in range(1,amount+1):
-            for c in coins:
-                if i-c >= 0:
-                    dp[i] = min(dp[i], 1 + dp[i-c])
-        return dp[amount] if dp[amount] != amount+1 else -1
+        
+        for i in range(1, len(dp)):
+            for j in coins:
+                if i-j >= 0:
+                    dp[i] = min(dp[i], dp[i-j]+1)
+        return dp[-1] if dp[-1] != float("inf") else -1
