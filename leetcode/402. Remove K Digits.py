dd10188b98626bc1db0e02a0420627b3bd9a39de
Doyeon Yun <61214927+govlehdus@users.noreply.github.com>
2024-04-11 21:51:03 -0700

    Create 402. Remove K Digits.py

---
+++ leetcode/402. Remove K Digits.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        stack = []
+        for digit in num:
+            while k > 0 and stack and stack[-1] > digit:
+                stack.pop()
+                k -= 1
+            stack.append(digit)
+        final_stack = stack[:-k] if k else stack
+        return ''.join(final_stack).lstrip('0') or '0'
