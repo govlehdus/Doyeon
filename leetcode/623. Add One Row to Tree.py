0acaf0198bc63a1531d1b82948469e305d36dc15
Doyeon Yun <61214927+govlehdus@users.noreply.github.com>
2024-04-16 10:13:30 -0700

    Update 623. Add One Row to Tree.py

--- leetcode/623. Add One Row to Tree.py
+++ leetcode/623. Add One Row to Tree.py
@@ -1,35 +1,35 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
 class Solution:
-    def add(self, root, val, depth, curr):
-        if not root:
-            return None
+    def addOneRow(self, root: Optional[TreeNode], val: int, depth: int) -> Optional[TreeNode]:
+        def add( root, val, depth, curr):
+            if not root:
+                return None
 
-        if curr == depth - 1:
-            lTemp = root.left
-            rTemp = root.right
+            if curr == depth - 1:
+                lTemp = root.left
+                rTemp = root.right
 
-            root.left = TreeNode(val)
-            root.right = TreeNode(val)
-            root.left.left = lTemp
-            root.right.right = rTemp
+                root.left = TreeNode(val)
+                root.right = TreeNode(val)
+                root.left.left = lTemp
+                root.right.right = rTemp
+
+                return root
+
+            root.left = add(root.left, val, depth, curr + 1)
+            root.right = add(root.right, val, depth, curr + 1)
 
             return root
 
-        root.left = self.add(root.left, val, depth, curr + 1)
-        root.right = self.add(root.right, val, depth, curr + 1)
 
-        return root
-
-    def addOneRow(self, root, val, depth):
         if depth == 1:
             newRoot = TreeNode(val)
             newRoot.left = root
             return newRoot
 
-        return self.add(root, val, depth, 1)
-           
+        return add(root, val, depth, 1)
